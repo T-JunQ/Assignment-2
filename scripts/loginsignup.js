0a72b7e1a9cc2afb231a0001a035dd9564646972
T-JunQ <s10240058@connect.np.edu.sg>
2023-01-31 14:48:53 +0800

    Scripted login and signup

--- scripts/loginsignup.js
+++ scripts/loginsignup.js
@@ -1,9 +1,16 @@
 $("document").ready(function () {
   let apikey = "63b648a1969f06502871aa39";
+  let rmbme = localStorage.getItem("rememberme");
+  if (rmbme == "true") {
+    $("#remember_me").prop("checked", true);
+    $("#login_email").val(localStorage.getItem("email"));
+    $("#login_pwd").val(localStorage.getItem("pwd"));
+  }
+
   $("#signup").submit(function (e) {
     e.preventDefault();
-    let email = $("#signup_email");
-    let name = $("#signup_name");
+    let email = $("#signup_email").val();
+    let name = $("#signup_user").val();
 
     let pwd = $("#signup_pwd").val();
     let pwd2 = $("#signup_pwd2").val();
@@ -14,13 +21,35 @@ $("document").ready(function () {
     }
   });
 
+  $("#login").submit(function (e) {
+    e.preventDefault();
+    let email = $("#login_email").val();
+    let pwd = $("#login_pwd").val();
+    if ($("#remember_me").is(":checked")) {
+      let email = $("#login_email").val();
+      let pwd = $("#login_pwd").val();
+      localStorage.setItem("rememberme", true);
+      localStorage.setItem("email", email);
+      localStorage.setItem("pwd", pwd);
+    } else {
+      localStorage.setItem("rememberme", false);
+      localStorage.removeItem("email");
+      localStorage.removeItem("pwd");
+    }
+    matchAccounts(email, pwd);
+  });
+
   function createAccount(email, username, password) {
     var jsondata = {
       email: email,
       username: username,
       password: password,
-      balance: "0.00",
+      picture: null,
+      balance: 0.0,
+      inventory: {},
     };
+
+    console.log(jsondata);
     var settings = {
       async: true,
       crossDomain: true,
@@ -34,14 +63,66 @@ $("document").ready(function () {
       processData: false,
       data: JSON.stringify(jsondata),
       beforeSend: function () {
-        $("#signup_submit").html(
-          `<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ht6o1bdu.json"  background="transparent"  speed="1"  style="width: 80px; height: 80px;"  loop  autoplay></lottie-player>`
+        $("#submit_button").html(
+          `<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ht6o1bdu.json"  background="transparent"  speed="1"  style="width: 100px; height: 100px; margin-left:auto; margin-right:auto;"  loop  autoplay></lottie-player>`
+        );
+      },
+      error: function () {
+        alert(
+          "Unable to create an account.\nEither this email address or username is already in use."
+        );
+        $("#submit_button").html(
+          `<button type="submit" class="btn btn-default" id="signup_submit">Sign Up</button>`
         );
       },
     };
     $.ajax(settings).done(function (response) {
       console.log(response);
       $("#sucess").modal("show");
+      $("#sucess").on("hidden.bs.modal", function () {
+        sessionStorage.setItem("Account", response);
+        window.location.href = "../templates/marketplace.html";
+      });
+    });
+  }
+
+  function matchAccounts(email, password) {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://interactivedev-f58c.restdb.io/rest/accounts",
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        "x-apikey": apikey,
+        "cache-control": "no-cache",
+      },
+      beforeSend: function () {
+        $("#login_submit").html(
+          `<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ht6o1bdu.json"  background="transparent"  speed="1"  style="width: 100px; height: 100px; margin-left:auto; margin-right:auto;"  loop  autoplay></lottie-player>`
+        );
+      },
+    };
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      var logedin = false;
+      for (var i = 0; i < response.length; i++) {
+        if (response[i].email == email && response[i].password === password) {
+          logedin = true;
+          $("#login_submit").html(
+            `<lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ht6o1bdu.json"  background="transparent"  speed="1"  style="width: 100px; height: 100px; margin-left:auto; margin-right:auto;"  loop  autoplay></lottie-player>`
+          );
+          sessionStorage.setItem("Account", response[i]);
+          console.log(response[i]);
+          window.location.href = "../templates/marketplace.html";
+        }
+      }
+      if (logedin === false) {
+        alert("The email or password is incorrect");
+        $("#login_submit").html(
+          `<button type="submit" class="btn btn-default">Log In</button>`
+        );
+      }
     });
   }
 });
