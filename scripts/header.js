592e696ecaec4dcc3b3ba392f32a8328d7a915cf
T-JunQ <s10240058@connect.np.edu.sg>
2023-02-05 17:07:40 +0800

    Finished marketplace, Created cart page

--- scripts/header.js
+++ scripts/header.js
@@ -5,13 +5,21 @@ $(document).ready(function ($) {
   console.log(account.username);
   $("#nav_username").text(account.username);
   $("#nav_balance p").text(`$ ${account.balance}`);
+  $("#nav_profilepic").attr("src", account.picture);
   $("#nav_buttons a").each(function () {
     console.log(url);
     var linkPage = this.href;
     if (url == linkPage) {
       $(this).css({
-        "font-weight": "700",
+        "font-weight": "800",
         color: "white",
+      });
+      $(this).hover(function () {
+        $(this).css({
+          "animation-name": "buttons",
+          "animation-duration": "0.3s",
+          filter: "brightness(800%)",
+        });
       });
     }
   });
--- scripts/marketplace.js
+++ scripts/marketplace.js
@@ -1,19 +1,83 @@
 $("document").ready(function () {
   $("#includehtml").load("header.html");
   let apikey = "63b648a1969f06502871aa39";
+  getListings();
+
   $("#game_filter").change(function () {
-    let game = $(this).find(":selected").data("game");
+    filter();
+  });
+
+  $("#price_filter").change(function () {
+    filter();
+  });
+
+  $("#sortby_filter").change(function () {
+    let sortby = $(this).find(":selected").data("sortby");
+    let listings = $(".listing");
+    listings
+      .sort(function (a, b) {
+        let aprice = $(".item_price", a).text();
+        aprice = parseFloat(aprice.substring(1));
+        console.log(aprice);
+        let bprice = $(".item_price", b).text();
+        bprice = parseFloat(bprice.substring(1));
+        if (sortby == "asc") {
+          return aprice - bprice;
+        } else if (sortby == "desc") {
+          return bprice - aprice;
+        } else if (sortby == "") {
+          return 0;
+        }
+      })
+      .appendTo("#mp_listings");
+  });
+
+  $("#search_input").keyup(function (e) {
+    let input = $("#search_input").val().toUpperCase();
+    input = input.trim();
+    console.log(input);
+    let itemnames = $(".listing > .item_name");
+    itemnames.each(function () {
+      let name = $(this).text().toUpperCase();
+      if (name.indexOf(input) != -1) {
+        $(this).parent().show();
+      } else {
+        $(this).parent().hide();
+      }
+    });
+  });
+
+  $(".addtocart").each(function (i) {
+    i.click(function () {
+      console.log("text");
+    });
+  });
+
+  function filter() {
+    let start = parseFloat($("#price_filter").find(":selected").data("start"));
+    let end = parseFloat($("#price_filter").find(":selected").data("end"));
+    let game = $("#game_filter").find(":selected").data("game");
     console.log(game);
-    $(".listing game_name").each(function (n) {
-      if (n == game) {
+    let listings = $(".listing");
+    listings.each(function () {
+      let gamename = $(this).children(".game_name").text();
+      console.log(gamename);
+      let price = $(this).children(".item_price").text();
+      price = price.substring(1);
+      console.log(price);
+      if ((gamename == game) & (start < price) & (price < end)) {
         $(this).show();
       } else {
         $(this).hide();
       }
+      if (game == "any") {
+        if ((start < price) & (price < end)) {
+          $(this).show();
+        }
+      }
     });
-  });
-  getListings();
-  $();
+  }
+
   function getListings() {
     var settings = {
       async: true,
@@ -35,18 +99,23 @@ $("document").ready(function () {
       console.log(response);
       let content = "";
       for (var i = 0; i < response.length; i++) {
-        console.log(response[i].image[0]);
+        var price = parseFloat(response[i].price).toFixed(2);
         content = `
         ${content}
         <div class="listing" id="${response[i]._id}">
-        <h2 class="item_name">${response[i].name}</h2>
-        <h3 class="game_name">${response[i].game}</h3>
-        <div class="img_container">
-        <img class="img-thumbnail item_img img-fluid" src="https://interactivedev-f58c.restdb.io/media/63db6dbfaa8607500002bbfe" alt="itemimg">
-        </div>
-        <h2 class="item_price">$ ${response[i].price}</h2>
-        <button class="btn btn-primary" class="addtocart">Add to Cart <i class="fa-solid fa-cart-plus"></i></i></button>
-       </div>`;
+          <h2 class="item_name">${response[i].name}</h2>
+          <h3 class="game_name">${response[i].game}</h3>
+          <div class="img_container">
+            <img class="img-thumbnail item_img img-fluid" src="${response[i].image}" alt="itemimg" onerror="this.src='../pics/logo-no-background - Copy (2).png'">
+          </div>
+          <h2 class="item_price">$ ${price}</h2>
+          <button class="btn btn-primary" class="addtocart">Add to Cart <i class="fa-solid fa-cart-plus"></i></i></button>
+          <div class="seller">
+            <img class="seller_pic" src="${response[i].seller[0].picture}" alt="profilepic"
+                onerror="src='../pics/vecteezy_profile-icon-design-vector_5544718.jpg'"  width="15px" height="15px">
+            <h4 class="seller_name">${response[i].seller[0].username}</h4>
+          </div>
+        </div>`;
       }
       $("#mp_listings").html(content);
     });
